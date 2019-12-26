c287d9010c5b7c93d17a66adb9737192dd851a22
Pierre Nodet <nodet.pierre@gmail.com>
2019-12-26 18:21:56 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Reading Notes
+[![CC BY 4.0](https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg
+)](https://github.com/pierrenodet/thesis/blob/master/LICENSE)
+
+My thesis manuscript and my reading notes of papers for my PhD in Weakly Supervised Learning.
+
+<p align="center"><a href="https://pierrenodet.github.io/thesis"><B>website</B></a></p>
---
+++ docusaurus.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  title: 'thesis',
+  tagline: 'Manuscript and Reading Notes of my PhD',
+  url: 'https://pierrenodet.github.io/thesis',
+  baseUrl: '/thesis/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'pierrenodet',
+  projectName: 'thesis',
+  themeConfig: {
+    navbar: {
+      title: 'thesis',
+      logo: {
+        alt: 'thesis logo',
+        src: 'img/logo.png',
+      },
+      links: [
+        { to: 'manuscript/introduction', label: 'Manuscript', position: 'right' },
+        { to: 'notes', label: 'Reading Notes', position: 'right' },
+        {
+          href: 'https://github.com/pierrenodet/thesis',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} Pierre Nodet.`,
+    },
+    disableDarkMode: true
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          path: 'manuscript',
+          routeBasePath: 'manuscript',
+          include: ['**/*.md', '**/*.mdx'],
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/pierrenodet/thesis',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        blog: {
+          path: 'notes',
+          routeBasePath: 'notes',
+          include: ['**/*.md', '**/*.mdx'],
+        },
+      },
+    ],
+  ],
+};
